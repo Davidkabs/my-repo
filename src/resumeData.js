3f5578353c5f03e91f8c8de63e45b276f2be4619
Davidkabs <dkabangu14@gmail.com>
2022-06-20 15:38:50 +0200

    all

---
+++ src/resumeData.js
@@ -0,0 +1,32 @@
+let resumeData = {
+    "imagebaseurl":"https://avatars.githubusercontent.com/u/52856862?s=400&u=f50fe35903079b0fc452f45bc0e2639d3a080f59&v=4",
+    "name": "David K Tshibangu",
+    "role": "Junior System Engineer",
+    "roleDescription": " I like to describe myself as more of a backend developer than a frontend developer but my goal is to become a fullstack developer to be able to work on entire projects. Sovtech being a Tecgnology company working with the latest technology is, by far one of the best environment to start my carerr as a system engineer",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/david-tshibangu-kabangu-362698171/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Davidkabs",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"Twitter",
+          "url":"https://twitter.com/DaveTkabs",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"A Newly Graduate in System Engineering with strong academic understanding of IT system design from conception to implementation. Throughout my three years at Richfield Graduate Institute of Technology, I developed a passion for computer programming and computer systems management and I believe that there is always room to learn in this domain.",
+    "address":"Johanessburg, Gauteng(South Africa)",
+    "email":"dkabangu14@gmail.com",
+    //"website":"dkabangu14@gmail.com",
+  
+    
+    
+  }
+  
+  export default resumeData
